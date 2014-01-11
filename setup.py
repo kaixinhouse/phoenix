936d686ca4b3ef6811b78b318dfdb5437d486891
kaixinhouse <kaixinhouse@gmail.com>
2014-01-11 22:33:44 +0800

    project init

---
+++ setup.py
@@ -0,0 +1,22 @@
+#! _*_ coding:utf-8 _*_
+
+from setuptools import setup
+
+project = "phoniex"
+
+
+setup(
+    name = phoniex,
+    version = "0.1",
+    description = "phoniex is a SNS website base on Flash",
+    author = "KBoard",
+    author_email = "kaixinhouse@gmail.com",
+    packages = ["phoniex"],
+    include_package_data = True,
+    zip_safe = False,
+    install_requires = [
+        "Flash",
+	"Flash-SQLAlchemy",
+	"mysql-python",
+    ]
+)
